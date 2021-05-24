017e481ff11d3aa6e80eff00c9b6b6754d755ae6
Chase Terry <chase.terry87@gmail.com>
2021-05-24 00:22:01 -0500

    Added tests for footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="footer">
     <div class="container">
-      <div class="has-text-centered">&copy; MIXD 2021</div>
+      <ul class="footer-links has-text-centered">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/favorites">Favorites</router-link></li>
+      </ul>
+      <div class="columns mt-5 has-text-centered">
+        <div class="column is-half">
+          MIXD uses data from
+          <a
+            href="https://www.thecocktaildb.com/"
+            target="_blank"
+            data-testid="cocktail-db-link"
+          >
+            TheCocktailDB
+          </a>
+        </div>
+        <div class="column is-half" data-testid="copyright">
+          &copy; MIXD 2021
+        </div>
+      </div>
     </div>
   </div>
 </template>
---
+++ tests/unit/components/footer.spec.js
@@ -0,0 +1,30 @@
+import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import Footer from '@/components/Footer'
+
+const localVue = createLocalVue()
+
+describe('Footer', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(Footer, {
+      localVue,
+      stubs: {
+        RouterLink: RouterLinkStub
+      }
+    })
+  })
+
+  afterEach(() => {
+    wrapper.destroy()
+  })
+
+  it('renders correctly', () => {
+    expect(
+      wrapper.find('[data-testid="cocktail-db-link"]').exists()
+    ).toBeTruthy()
+    expect(wrapper.find('.footer-links').exists()).toBeTruthy()
+    expect(wrapper.find('[data-testid="copyright"]').exists()).toBeTruthy()
+    expect(wrapper.findAllComponents(RouterLinkStub).length).toEqual(2)
+  })
+})
